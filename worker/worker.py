b44c86895b875eb99316b628bef1b34841209d42
garynotethan <huangethan93@gmail.com>
2026-03-27 00:23:00 -0700

    feat: connect worker to db

--- worker/worker.py
+++ worker/worker.py
@@ -1,10 +1,33 @@
 import pika
 import json
+import os
+import psycopg2
+import time
+
+DB_URL = os.environ.get('DATABASE_URL')
 
 def process_pdf(ch, method, properties, body):
     data = json.loads(body)
     user_id = data['user_id']
     document_id = data['document_id']
+
+    time.sleep(3)# process here
+    try:
+        conn = psycopg2.connect(DB_URL)
+        cur = conn.cursor()
+        cur.execute(
+            "UPDATE documents SET status = 'ready' WHERE id = %s", 
+            (document_id)
+        )
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+    except Exception as e:
+        print(f"error processing document")
+        ch.basic_nack(delivery_tag=method.delivery_tag, multiple=False)
+
     
 def main():
     # host should be name of rabbitmq container i think
